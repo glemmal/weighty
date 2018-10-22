2896336185d17575b4f9c47ac8cd431ca6d18ae9
Elias Klemm <eliasklemm@gmail.com>
2018-10-22 12:59:38 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
+import { addWeight, removeWeight } from './store/actions/weightActions';
 import './App.css';
 
 class App extends Component {
+
+  addWeight = (event) => {
+    this.props.addWeight(300);
+  }
+
+  removeWeight = (event) => {
+    this.props.removeWeight(300);
+  }
+
+  onInputChange (event) {
+    this.props.addWeight(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +26,14 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <input placeholder="add some weight" onChange={this.onInputChange.bind(this)}/>
+          <button onClick={this.addWeight}>Add some weight!</button>
+          <button onClick={this.removeWeight}>Remove some weight!</button>
+          <ul>
+            {this.props.weights.map((v) => (
+              <li key={v}>{v}</li>
+            ))}
+          </ul>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -20,9 +43,23 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  addWeight: (weight) => dispatch(addWeight(weight)),
+  removeWeight: (weight) => dispatch(removeWeight(weight))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
